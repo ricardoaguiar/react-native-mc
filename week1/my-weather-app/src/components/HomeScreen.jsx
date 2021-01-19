ab2a61529196ae054a3220149f3c390458db1323
ricardoaguiar <barrios.dev@gmail.com>
2021-01-19 21:57:16 +0100

    inceased font size

--- week1/my-weather-app/src/components/HomeScreen.jsx
+++ week1/my-weather-app/src/components/HomeScreen.jsx
@@ -4,7 +4,7 @@ import { Text, View, StyleSheet } from 'react-native';
 export const HomeScreen = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontSize: 20 }}>Home Screen</Text>
+      <Text style={{ fontSize: 24 }}>Home Screen</Text>
     </View>
   );
 };
