47680db674d9fa63699296eac56d5a482999b2e4
ricardoaguiar <barrios.dev@gmail.com>
2021-01-19 22:01:30 +0100

    splash screen

--- week1/my-weather-app/src/components/HomeScreen.jsx
+++ week1/my-weather-app/src/components/HomeScreen.jsx
@@ -4,7 +4,8 @@ import { Text, View, StyleSheet } from 'react-native';
 export const HomeScreen = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontSize: 20 }}>Home Screen</Text>
+      <Text style={{ fontSize: 64 }}>🥳</Text>
+      <Text style={{ fontSize: 20 }}>Yay!</Text>
     </View>
   );
 };
