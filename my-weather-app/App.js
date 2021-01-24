60730f921040c02b0287d623948520b8a995bc7b
ricardoaguiar <barrios.dev@gmail.com>
2021-01-24 16:57:20 +0100

    home screen

--- my-weather-app/App.js
+++ my-weather-app/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import { ExerciseOne } from './src/components/ExerciseOne';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { HomeScreen } from './src/components/HomeScreen';
+import { WeeklyScreen } from './src/components/WeeklyScreen';
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
-  return <ExerciseOne />;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Weekly" component={WeeklyScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
